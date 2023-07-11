50e05c0ca7fbae9e7c51f5969811ce476e2f9d8d
olivertoussaint <46441581+olivertoussaint@users.noreply.github.com>
2023-07-11 17:34:32 +0200

    input data-testid fixed

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -37,12 +37,33 @@ export default class Login {
 
   }
 
+  // handleSubmitAdmin = e => {
+  //   e.preventDefault()
+  //   const user = {
+  //     type: "Admin",
+  //     email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
+  //     password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+  //     status: "connected"
+  //   }
+  //   this.localStorage.setItem("user", JSON.stringify(user))
+  //   this.login(user)
+  //     .catch(
+  //       (err) => this.createUser(user)
+  //     )
+  //     .then(() => {
+  //       this.onNavigate(ROUTES_PATH['Dashboard'])
+  //       this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
+  //       PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
+  //       document.body.style.backgroundColor="#fff"
+  //     })
+  // }
+
   handleSubmitAdmin = e => {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
